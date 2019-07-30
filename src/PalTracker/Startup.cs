cb3b9df1badea4686bca44aa3a50f3dda5b446f3
goriparthikartheek <goriparthi_kartheek@dell.com>
2019-07-30 18:07:57 +0530

    cloudNativeDeveloperDatabaseMigrations

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -38,7 +38,7 @@ namespace PalTracker
                 )
             );
 
-            services.AddTransient<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
