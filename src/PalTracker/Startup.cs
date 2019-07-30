aebaa19d58122ab1b9a5064437f865ad48a6f27b
goriparthikartheek <goriparthi_kartheek@dell.com>
2019-07-30 12:19:59 +0530

    time sheet

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -37,6 +37,8 @@ namespace PalTracker
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR", "Test5000")
                 )
             );
+
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
